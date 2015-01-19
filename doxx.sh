77fd506efdff69b0073a1430928ed808a48a7f35
escudero89 <escudero89@gmail.com>
2015-01-18 22:40:34 -0300

    Moved doxx

--- doxx.sh
+++
@@ -1,3 +0,0 @@
-# Generates the documentation of core/qml/docs/js (Javascript) by using Doxx
-# Requires doxx (npm install doxx -g)
-doxx --source core/qml/docs/js --ignore third-party --target tech-docs/doxx
