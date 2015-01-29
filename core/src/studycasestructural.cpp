fd7e56792b683a8edf670cc69a26f1198f5f7efb
Cristian Escudero <escudero89@gmail.com>
2015-01-29 20:47:07 -0300

    Added comments in study case structural

--- core/src/studycasestructural.cpp
+++ core/src/studycasestructural.cpp
@@ -8,6 +8,9 @@
 StudyCaseStructural::StudyCaseStructural() {
 }
 
+/**
+ * @brief Sets some properties related to the heat transfer problem.
+ */
 void StudyCaseStructural::setLocalMapOfInformation() {
 
     m_mapOfInformation["youngModulus"]           = "false";
@@ -18,12 +21,29 @@ void StudyCaseStructural::setLocalMapOfInformation() {
 
 }
 
+/**
+ * @brief Saves and splits the configuration file
+ *
+ * The difference between this and StudyCaseHeat::saveLocalCurrentConfiguration()
+ * is the filter used when selecting the blocks from the configuration file.
+ * Here we are using `MAT-fem`
+ *
+ */
 void StudyCaseStructural::saveLocalCurrentConfiguration() {
     QStringList configurationFilter;
     configurationFilter << "MAT-variables" << "MAT-fem";
     FileIO::splitConfigurationFile("currentMatFemFile.m", m_source, configurationFilter, true);
 }
 
+/**
+ * @brief Check if the StudyCase is ready for MATfem
+ *
+ * Since there are certain conditions that allows MATfem to solve the problem,
+ * such as having a Poisson's coefficient between 0 and 0.5, we check if this
+ * conditions are met before calling the interpreter.
+ *
+ * @return Whether the conditions are met or not
+ */
 bool StudyCaseStructural::checkIfReady() {
 
     QStringList variablesToCheck;
--- core/src/studycasestructural.h
+++ core/src/studycasestructural.h
@@ -3,6 +3,13 @@
 
 #include "studycase.h"
 
+/**
+ * @brief The StudyCaseStructural class that inherits of StudyCase
+ *
+ * Since there are currently two main types of Study Cases, one for heat
+ * transfer and another one for structural analysis, there's a need for a
+ * clear separation between those subclasses.
+ */
 class StudyCaseStructural : public virtual StudyCase
 {
 public:
